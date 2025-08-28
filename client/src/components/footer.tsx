071d16443e9bcdb9128dde2a4e76099be85282e8
eeniemark <1173361407@qq.com>
2025-08-28 08:45:32 +0800

    Update footer.tsx

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -59,7 +59,7 @@ function Footer() {
                             setDoubleClickTimes(doubleClickTimes + 1)
                         }
                     }}>
-                        © {new Date().getFullYear()} Powered by <a className='hover:underline' href="https://github.com/openRin/Rin" target="_blank">Rin</a>
+                        © 2024 - {new Date().getFullYear()} Powered by <a className='hover:underline' href="https://www.980703.xyz" target="_blank">PuPu</a>
                     </span>
                     {config.get<boolean>('rss') && <>
                         <Spliter />
